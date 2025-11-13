1c62e5148c998507acd4a79421f2e58a0dc31c00
Nguyễn Văn Toại <122630929+tojinguyen@users.noreply.github.com>
2025-11-13 20:38:01 +0700

    feat: update model user

--- internal/models/user-auth/user.go
+++ internal/models/user-auth/user.go
@@ -7,6 +7,23 @@ import (
 	"gorm.io/gorm"
 )
 
+type Gender string
+
+const (
+	Male   Gender = "male"
+	Female Gender = "female"
+	Other  Gender = "other"
+)
+
+type Role string
+
+type LoginProvider string
+
+const (
+	Local          LoginProvider = "local"
+	GoogleProvider LoginProvider = "google"
+)
+
 type User struct {
 	gorm.Model
 	ID uuid.UUID `gorm:"type:uuid;default:gen_random_uuid();primaryKey"`
@@ -16,20 +33,15 @@ type User struct {
 	Username     string `gorm:"uniqueIndex;not null"`
 	PasswordHash string `gorm:"not null"`
 	Phone        string `gorm:"type:varchar(20)"`
-	IsVerified   bool   `gorm:"default:false"`
-	Status       string `gorm:"type:varchar(20);default:'ACTIVE'"` // ACTIVE, BANNED, PENDING, DELETED
 
 	// Profile
 	FullName    string `gorm:"type:varchar(255)"`
 	AvatarURL   string `gorm:"type:text"`
-	Bio         string `gorm:"type:text"`
 	DateOfBirth *time.Time
-	Gender      string `gorm:"type:varchar(20)"`
+	Gender      Gender `gorm:"type:varchar(20)"`
 	Address     string `gorm:"type:text"`
 
 	// Auth & Role
-	Role                string `gorm:"type:varchar(50);default:'USER'"`  // ADMIN, USER, MODERATOR, etc.
-	LoginProvider       string `gorm:"type:varchar(50);default:'local'"` // local, google, github, etc.
-	LastLoginAt         *time.Time
-	RefreshTokenVersion int `gorm:"default:1"`
+	LoginProvider LoginProvider `gorm:"type:varchar(50);default:'local'"`
+	LastLoginAt   *time.Time
 }
